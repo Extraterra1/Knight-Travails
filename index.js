840addcedd349a95e1b8c5e00ad5d3313dda9f34
Carlos Pires <extraterra1@gmail.com>
2023-10-05 11:54:30 +0100

    add board builder

---
+++ index.js
@@ -0,0 +1,13 @@
+const knightMoves = (start, end) => {
+  const possibleMovesX = [1, 2, 2, 1, -1, -2, -2, -1];
+  const possibleMovesY = [2, 1, -1, -2, -2, -1, 1, 2];
+
+  const validMove = (x, y) => x >= 0 && y >= 0 && x < 8 && y < 8;
+
+  const queue = [{ x: start[0], y: start[1], path: start }];
+  const board = new Array(8).fill(0).map((item) => new Array(8).fill(false));
+
+  while (queue.length > 0) {
+    const { x, y, path } = queue.shift();
+  }
+};
